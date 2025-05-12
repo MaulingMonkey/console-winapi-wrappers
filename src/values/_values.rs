ce02ea4fce1a89e5db74a85346f8a48572af3ad4
MaulingMonkey <git@maulingmonkey.com>
2025-05-12 11:49:16 -0700

    src\values\_values.rs: whitespace

--- src/values/_values.rs
+++ src/values/_values.rs
@@ -1,10 +1,10 @@
 //! \[[microsoft.com](https://learn.microsoft.com/en-us/windows/console/console-structures)\]
 
-mod ascii_or_unicode_char;      pub use ascii_or_unicode_char::*;
-mod char_info;                  pub use char_info::*;
-mod color_ref;                  pub use color_ref::*;
-mod console_cursor_info;        pub use console_cursor_info::*;
-mod console_screen_buffer_info; pub use console_screen_buffer_info::*;
-mod console_screen_buffer_info_ex; pub use console_screen_buffer_info_ex::*;
-mod coord;                      pub use coord::*;
-mod small_rect;                 pub use small_rect::*;
+mod ascii_or_unicode_char;              pub use ascii_or_unicode_char::*;
+mod char_info;                          pub use char_info::*;
+mod color_ref;                          pub use color_ref::*;
+mod console_cursor_info;                pub use console_cursor_info::*;
+mod console_screen_buffer_info;         pub use console_screen_buffer_info::*;
+mod console_screen_buffer_info_ex;      pub use console_screen_buffer_info_ex::*;
+mod coord;                              pub use coord::*;
+mod small_rect;                         pub use small_rect::*;
